01c664ec6f0cc433c3443396d9ad0064859dca73
Dinara Gaynutdinova <dinarakiss84@gmail.com>
2022-02-17 22:12:23 +0300

    3

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,18 +1,19 @@
 const button = document.getElementById("tasks__add")
 const tasksList = document.getElementById("tasks__list")
-let taskInput = document.getElementById("task__input")
+const taskInput = document.getElementById("task__input")
 
 button.addEventListener("click", (evt) => {
-    const inputedText = taskInput.value.trim() 
+    const inputedText = taskInput.value.trim()
     if (inputedText !== "") {
-        //evt.preventDefault()
         tasksList.insertAdjacentHTML("afterbegin",`<div class='task'><div class='task__title'>${inputedText}</div><a href='#' class='task__remove'>&times;</a></div>`)
-        taskInput.value = ""
-        evt.preventDefault()
-        document.querySelector(".task__remove").addEventListener("click", () => {
-           evt.target.closest(".task").remove()
-        })
-    } else {
-        evt.preventDefault()
+        taskInput.value = ""  
     }
+    const tasksRemove = Array.from(document.querySelectorAll(".task__remove"))
+    for (let taskRemove of tasksRemove) {
+        taskRemove.addEventListener("click", () => {
+            taskRemove.closest(".task").remove()})
+    }
+    evt.preventDefault() 
 })
+
+
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -9,6 +9,7 @@ strings.forEach((item) => {
     tooltip.style.top = coordinates.bottom + 7 + 'px';
         if (tooltip.classList.contains('tooltip_active')) {
             tooltip.classList.remove('tooltip_active');
+            evt.preventDefault()
         } else {
             Array.from(document.querySelectorAll(".tooltip_active")).forEach((elem) => elem.classList.remove("tooltip_active"))
             tooltip.classList.add('tooltip_active');
